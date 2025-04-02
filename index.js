e1949a55703f284cf283430a0d82052a10b1613d
preethramadoss <167985196+preethramadoss@users.noreply.github.com>
2025-04-02 12:54:10 +1100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
         </style>
       </head>
       <body>
-        <h1>Welcome to LOOPII World ✨</h1>
+        <h1>Welcome to LOOPII World </h1>
         <img src="/doll.gif" alt="Dancing Doll" />
       </body>
       </html>
